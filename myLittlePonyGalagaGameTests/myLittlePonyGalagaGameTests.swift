64610d2e33a128de96693cb22be930259c4b4e4f
cameron bradford <kylocam0712@gmail.com>
2025-02-28 08:06:37 -0600

    Initial Commit

---
+++ myLittlePonyGalagaGameTests/myLittlePonyGalagaGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  myLittlePonyGalagaGameTests.swift
+//  myLittlePonyGalagaGameTests
+//
+//  Created by CAMERON BRADFORD on 2/28/25.
+//
+
+import Testing
+@testable import myLittlePonyGalagaGame
+
+struct myLittlePonyGalagaGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
